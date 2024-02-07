80b58e1acca331d4e9ec22ac956593cd7204eef5
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2024-02-07 09:40:41 +0800

    -added top_n boxes -added snomed ct and icd function

--- app/page.js
+++ app/page.js
@@ -8,17 +8,23 @@ export default function Home() {
   const [text, setText] = useState('');
   const [snomedResponse, setSnomedResponse] = useState(null);
   const [fetching, setFetching] = useState(false);
+  const [apiSelection, setApiSelection] = useState('SNOMEDCT');
+  const [topN, setTopN] = useState(5); // State for 'top_n' value
 
   const handleSubmit = async () => {
     console.log(text);
 
     if (text.trim() === '') {
-      return alert('Please enter a valid procedure name');
+      return alert(`Please enter a valid ${apiSelection === 'SNOMEDCT' ? 'procedure' : 'diagnosis'} name`);
     }
+
+    const apiUrl = apiSelection === 'SNOMEDCT'
+                   ? process.env.NEXT_PUBLIC_API_SNOMEDCT_URL
+                   : process.env.NEXT_PUBLIC_API_ICD_URL;
 
     const dataToSend = {
       text: text,
-      top_n: 5 // or any other number you wish to specify
+      top_n: topN
     };
 
     setSnomedResponse(null);
@@ -26,7 +32,7 @@ export default function Home() {
     try {
       setFetching(true);
 
-      const res = await axios.post(process.env.NEXT_PUBLIC_API_URL, dataToSend, {
+      const res = await axios.post(apiUrl, dataToSend, {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
@@ -47,17 +53,51 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Malaysian Bot Record Optimizer (MyBRO) Demo
+          Malaysian Bot Record Optimizer (MyBRO) Demo - AI assisted coding service
         </p>
       </div>
 
       <div className='w-full max-w-5xl mb-8'>
-        <label htmlFor="procedureInput" className="mb-2 text-lg">Procedure:</label>
+        <div className='flex justify-center mb-4 items-center'>
+          {/* SNOMEDCT and ICD10 buttons */}
+          <button
+            className={`mx-2 p-2 ${apiSelection === 'SNOMEDCT' ? 'bg-blue-500 text-white' : 'bg-black text-white'}`}
+            onClick={() => setApiSelection('SNOMEDCT')}
+          >
+            SNOMEDCT
+          </button>
+          <button
+            className={`mx-2 p-2 ${apiSelection === 'ICD10' ? 'bg-blue-500 text-white' : 'bg-black text-white'}`}
+            onClick={() => setApiSelection('ICD10')}
+          >
+            ICD10
+          </button>
+
+          {/* Spacer */}
+            <div className='w-8'></div>
+
+          {/* Input for 'top_n' value */}
+          <div className='flex items-center'>
+            <label htmlFor="topNInput" className="mr-2 text-lg">top_n:</label>
+            <input
+                id="topNInput"
+                type="number"
+                className='p-2 w-20 border-2 border-gray-300 rounded-lg bg-white text-black'
+                value={topN}
+                onChange={(e) => setTopN(Math.min(100, Math.max(1, parseInt(e.target.value || 0))))}
+                min="1"
+                max="100"
+                placeholder="Top N"
+            />
+          </div>
+        </div>
+
+          <label htmlFor="procedureInput" className="mb-2 text-lg">{apiSelection === 'SNOMEDCT' ? 'Procedure:' : 'Diagnosis:'}</label>
         <textarea
           id="procedureInput"
           className='w-full h-40 p-4 border-2 text-black border-gray-300 rounded-lg dark:border-neutral-800'
           value={text}
-          placeholder='Enter procedure name here'
+          placeholder={apiSelection === 'SNOMEDCT' ? 'Enter procedure name here' : 'Enter your diagnosis here'}
           onChange={(e) => setText(e.target.value)}
         ></textarea>
         <button
@@ -73,7 +113,7 @@ export default function Home() {
       <div className='w-full max-w-5xl'>
         <div className='rounded-lg border border-transparent px-5 py-4 transition-colors'>
           <h2 className={`text-2xl font-semibold mb-4`}>
-            Table{' '}
+            Prediction Table based on F1 score{' '}
           </h2>
           <div className='overflow-x-auto'>
             {snomedResponse ? (
